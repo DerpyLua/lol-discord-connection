13da83d81761bff0e6ffedcb26de453d709fb42b
DerpyLua <60990357+DerpyLua@users.noreply.github.com>
2020-02-12 17:46:45 -0500

    Add files via upload

---
+++ Discord League Name Adder/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Discord_League_Name_Adder
+{
+	// Token: 0x02000002 RID: 2
+	public partial class Form1 : global::DarkUI.Forms.DarkForm
+	{
+		// Token: 0x0600000B RID: 11 RVA: 0x00002238 File Offset: 0x00000438
+		protected override void Dispose(bool disposing)
+		{
+			bool flag = disposing && this.components != null;
+			if (flag)
+			{
+				this.components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		// Token: 0x0600000C RID: 12 RVA: 0x00002270 File Offset: 0x00000470
+		private void InitializeComponent()
+		{
+			this.darkButton1 = new global::DarkUI.Controls.DarkButton();
+			this.darkButton2 = new global::DarkUI.Controls.DarkButton();
+			this.TextBoxUsername = new global::DarkUI.Controls.DarkTextBox();
+			this.darkLabel1 = new global::DarkUI.Controls.DarkLabel();
+			this.TextBoxAuth = new global::DarkUI.Controls.DarkTextBox();
+			this.darkLabel2 = new global::DarkUI.Controls.DarkLabel();
+			base.SuspendLayout();
+			this.darkButton1.Location = new global::System.Drawing.Point(162, 2);
+			this.darkButton1.Name = "darkButton1";
+			this.darkButton1.Padding = new global::System.Windows.Forms.Padding(5);
+			this.darkButton1.Size = new global::System.Drawing.Size(32, 23);
+			this.darkButton1.TabIndex = 0;
+			this.darkButton1.Text = "X";
+			this.darkButton1.Click += new global::System.EventHandler(this.DarkButton1_Click);
+			this.darkButton2.Location = new global::System.Drawing.Point(59, 184);
+			this.darkButton2.Name = "darkButton2";
+			this.darkButton2.Padding = new global::System.Windows.Forms.Padding(5);
+			this.darkButton2.Size = new global::System.Drawing.Size(78, 38);
+			this.darkButton2.TabIndex = 1;
+			this.darkButton2.Text = "Add";
+			this.darkButton2.Click += new global::System.EventHandler(this.DarkButton2_Click);
+			this.TextBoxUsername.BackColor = global::System.Drawing.Color.FromArgb(69, 73, 74);
+			this.TextBoxUsername.BorderStyle = global::System.Windows.Forms.BorderStyle.FixedSingle;
+			this.TextBoxUsername.ForeColor = global::System.Drawing.Color.FromArgb(220, 220, 220);
+			this.TextBoxUsername.Location = new global::System.Drawing.Point(14, 65);
+			this.TextBoxUsername.Name = "TextBoxUsername";
+			this.TextBoxUsername.Size = new global::System.Drawing.Size(141, 20);
+			this.TextBoxUsername.TabIndex = 2;
+			this.darkLabel1.AutoSize = true;
+			this.darkLabel1.ForeColor = global::System.Drawing.Color.FromArgb(220, 220, 220);
+			this.darkLabel1.Location = new global::System.Drawing.Point(11, 29);
+			this.darkLabel1.Name = "darkLabel1";
+			this.darkLabel1.Size = new global::System.Drawing.Size(94, 13);
+			this.darkLabel1.TabIndex = 3;
+			this.darkLabel1.Text = "Desired Username";
+			this.TextBoxAuth.BackColor = global::System.Drawing.Color.FromArgb(69, 73, 74);
+			this.TextBoxAuth.BorderStyle = global::System.Windows.Forms.BorderStyle.FixedSingle;
+			this.TextBoxAuth.ForeColor = global::System.Drawing.Color.FromArgb(220, 220, 220);
+			this.TextBoxAuth.Location = new global::System.Drawing.Point(14, 143);
+			this.TextBoxAuth.Name = "TextBoxAuth";
+			this.TextBoxAuth.Size = new global::System.Drawing.Size(141, 20);
+			this.TextBoxAuth.TabIndex = 4;
+			this.darkLabel2.AutoSize = true;
+			this.darkLabel2.ForeColor = global::System.Drawing.Color.FromArgb(220, 220, 220);
+			this.darkLabel2.Location = new global::System.Drawing.Point(11, 112);
+			this.darkLabel2.Name = "darkLabel2";
+			this.darkLabel2.Size = new global::System.Drawing.Size(68, 13);
+			this.darkLabel2.TabIndex = 5;
+			this.darkLabel2.Text = "Authorization";
+			base.AutoScaleDimensions = new global::System.Drawing.SizeF(6f, 13f);
+			base.AutoScaleMode = global::System.Windows.Forms.AutoScaleMode.Font;
+			base.ClientSize = new global::System.Drawing.Size(195, 244);
+			base.Controls.Add(this.darkLabel2);
+			base.Controls.Add(this.TextBoxAuth);
+			base.Controls.Add(this.darkLabel1);
+			base.Controls.Add(this.TextBoxUsername);
+			base.Controls.Add(this.darkButton2);
+			base.Controls.Add(this.darkButton1);
+			base.FormBorderStyle = global::System.Windows.Forms.FormBorderStyle.None;
+			base.Name = "Form1";
+			base.StartPosition = global::System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			base.Load += new global::System.EventHandler(this.Form1_Load);
+			base.MouseDown += new global::System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+			base.ResumeLayout(false);
+			base.PerformLayout();
+		}
+
+		// Token: 0x04000004 RID: 4
+		private global::System.ComponentModel.IContainer components = null;
+
+		// Token: 0x04000005 RID: 5
+		private global::DarkUI.Controls.DarkButton darkButton1;
+
+		// Token: 0x04000006 RID: 6
+		private global::DarkUI.Controls.DarkButton darkButton2;
+
+		// Token: 0x04000007 RID: 7
+		private global::DarkUI.Controls.DarkTextBox TextBoxUsername;
+
+		// Token: 0x04000008 RID: 8
+		private global::DarkUI.Controls.DarkLabel darkLabel1;
+
+		// Token: 0x04000009 RID: 9
+		private global::DarkUI.Controls.DarkTextBox TextBoxAuth;
+
+		// Token: 0x0400000A RID: 10
+		private global::DarkUI.Controls.DarkLabel darkLabel2;
+	}
+}
---
+++ Discord League Name Adder/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Windows.Forms;
+using DarkUI.Controls;
+using DarkUI.Forms;
+
+namespace Discord_League_Name_Adder
+{
+	// Token: 0x02000002 RID: 2
+	public partial class Form1 : DarkForm
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+		public Form1()
+		{
+			this.InitializeComponent();
+		}
+
+		// Token: 0x06000002 RID: 2
+		[DllImport("user32.dll")]
+		public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+		// Token: 0x06000003 RID: 3
+		[DllImport("user32.dll")]
+		public static extern bool ReleaseCapture();
+
+		// Token: 0x06000004 RID: 4 RVA: 0x00002068 File Offset: 0x00000268
+		private void Form1_Load(object sender, EventArgs e)
+		{
+		}
+
+		// Token: 0x06000005 RID: 5 RVA: 0x0000206C File Offset: 0x0000026C
+		public static string RandomString(int length)
+		{
+			return new string((from s in Enumerable.Repeat<string>("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", length)
+			select s[Form1.random.Next(s.Length)]).ToArray<char>());
+		}
+
+		// Token: 0x06000006 RID: 6 RVA: 0x000020B8 File Offset: 0x000002B8
+		private void Form1_MouseDown(object sender, MouseEventArgs e)
+		{
+			bool flag = e.Button == MouseButtons.Left;
+			if (flag)
+			{
+				Form1.ReleaseCapture();
+				Form1.SendMessage(base.Handle, 161, 2, 0);
+			}
+		}
+
+		// Token: 0x06000007 RID: 7 RVA: 0x000020F4 File Offset: 0x000002F4
+		public void PutObject(string postUrl, string postdata, string second)
+		{
+			try
+			{
+				WebRequest webRequest = WebRequest.Create("https://discordapp.com/api/v6/users/@me/connections/leagueoflegends/authorize");
+				webRequest.Method = "PUT";
+				byte[] bytes = Encoding.UTF8.GetBytes(postdata);
+				webRequest.ContentType = "application/json";
+				webRequest.ContentLength = (long)bytes.Length;
+				webRequest.Headers.Add("Authorization", second);
+				Stream stream = webRequest.GetRequestStream();
+				stream.Write(bytes, 0, bytes.Length);
+				stream.Close();
+				WebResponse response = webRequest.GetResponse();
+				Stream responseStream;
+				stream = (responseStream = response.GetResponseStream());
+				try
+				{
+					StreamReader streamReader = new StreamReader(stream);
+					string text = streamReader.ReadToEnd();
+				}
+				finally
+				{
+					if (responseStream != null)
+					{
+						((IDisposable)responseStream).Dispose();
+					}
+				}
+				response.Close();
+			}
+			catch
+			{
+			}
+		}
+
+		// Token: 0x06000008 RID: 8 RVA: 0x000021D4 File Offset: 0x000003D4
+		public void Serialize(Stream output, object input)
+		{
+			DataContractSerializer dataContractSerializer = new DataContractSerializer(input.GetType());
+			dataContractSerializer.WriteObject(output, input);
+		}
+
+		// Token: 0x06000009 RID: 9 RVA: 0x000021F7 File Offset: 0x000003F7
+		private void DarkButton2_Click(object sender, EventArgs e)
+		{
+			this.PutObject("", "{\"name\":\"" + this.TextBoxUsername.Text + "\",\"friend_sync\":true}", this.TextBoxAuth.Text);
+		}
+
+		// Token: 0x0600000A RID: 10 RVA: 0x0000222B File Offset: 0x0000042B
+		private void DarkButton1_Click(object sender, EventArgs e)
+		{
+			Environment.Exit(0);
+		}
+
+		// Token: 0x04000001 RID: 1
+		public const int WM_NCLBUTTONDOWN = 161;
+
+		// Token: 0x04000002 RID: 2
+		public const int HT_CAPTION = 2;
+
+		// Token: 0x04000003 RID: 3
+		private static Random random = new Random();
+	}
+}
